150384d286267beff5526426e00f757fc213f72b
Rafael <rafa.teirico@gmail.com>
2021-06-14 13:41:19 -0300

    feito

---
+++ src/java/web/DbListener.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+import db.User;
+import java.sql.*;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Web application lifecycle listener.
+ *
+ * @author palom
+ */
+public class DbListener implements ServletContextListener {
+    public static final String CLASS_NAME = "org.sqlite.JDBC";
+    public static final String URL = "jdbc:sqlite:p2.db";
+    
+    public static String step = null;
+    public static Exception exception = null;
+    
+    public static Connection getConnection() throws Exception{
+        return DriverManager.getConnection(URL);
+    }
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try{
+            step = "Conectando com a base";
+            Class.forName(CLASS_NAME);
+            Connection con = getConnection();
+            Statement stmt = con.createStatement();
+            //stmt.execute("DROP TABLE users");
+            step = "Criando tabela de usuários";
+            String sql = "CREATE TABLE IF NOT EXISTS users("
+                    + "name VARCHAR(200) NOT NULL,"
+                    + "login VARCHAR(50) UNIQUE NOT NULL,"
+                    + "password_hash LONG,"
+                    + "role VARCHAR(20) NOT NULL"
+                    + ")";
+            stmt.execute(sql);
+            if(User.getUsers().isEmpty()){
+                step = "Inserindo usuário administrativo";
+                sql = "INSERT INTO users(name, login, password_hash, role) "
+                    + "VALUES('Administrador', 'admin', '"+("1234".hashCode())+"', 'ADMIN')";
+                stmt.execute(sql);
+                sql = "INSERT INTO users(name, login, password_hash, role) "
+                    + "VALUES('Paloma Ierardi', 'paloma', '"+("1234".hashCode())+"', 'USER')";
+                stmt.execute(sql); 
+            }
+            step = "Criando tabela de disciplinas - nome, dia da semana, horário, quantidade de aulas, nota da p1 e nota da p2";
+            sql = "CREATE TABLE IF NOT EXISTS disciplinas("
+                    + "nome VARCHAR(50) PRIMARY KEY,"
+                    + "diadasemana VARCHAR(200) NOT NULL,"
+                    + "horario VARCHAR(200) NOT NULL,"
+                    + "qtaulas VARCHAR(200) NOT NULL,"
+                    + "notap1 double NOT NULL,"
+                    + "notap2 double NOT NULL"
+                    + ")";
+            stmt.execute(sql);
+           
+                
+            
+            //TODO: implementar código de criação das outras tabelas
+            stmt.close();
+            con.close();
+        }catch(Exception ex){
+            exception = ex;
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,48 @@
+<%-- 
+    Document   : index
+    Created on : 12 de jun. de 2021, 20:10:57
+    Author     : Rafael
+--%><%@page import="db.Disciplinas"%>
+
+
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <!--Um quadro com seu número de matrícula, nome completo, semestre em que ingressou na Fatec e o link de seu perfil no GitHub-->
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Ínicio</title>
+    </head>
+    <body>
+        <%@include file="WEB-INF/jspf/header.jspf" %>
+        <h1>RAFAEL CAETANO TEIXEIRA</h1>
+        <h2>RA: 1290481922039</h2>
+        <h3>Semestre de ingresso na Fatec: 08/2019</h3>
+        <a href="https://github.com/RafaelCaetanoTeixeira">Github</a>
+        
+        <hr/>
+        
+        <% String login = (String) session.getAttribute("user.login");%>
+        <%if(login == null){%>
+            <div>Esta página é restrita a usuários logados.</div>
+        <%}else{%>
+        <table border="1">
+            <tr>
+                <th>Nome</th>
+                <th>Nota P1</th>
+                <th>Nota P2</th>
+                <th>Média</th>
+            </tr>
+            <%for(Disciplinas disciplina: Disciplinas.getDisciplinas()){%>
+                <tr>
+                    <td><%= disciplina.getNome()%></td>
+                    <td><%= disciplina.getNotap1()%></td>
+                    <td><%= disciplina.getNotap2()%></td>
+                    <td> <%=(disciplina.getNotap1() + disciplina.getNotap2()) / 2 %></td>
+                </tr>
+            <%}%>
+        </table>
+        <%}%>
+    </body>
+</html>
